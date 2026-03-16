85ffca6abc36cb882b5ab8418e8cdca0be24b0a0
BindhuBadeti <bindhu.b@dhatvibs.com>
2026-03-16 15:11:16 +0530

    user api's code adding

--- src/main/java/com/dhatvibs/dashboard/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dhatvibs/dashboard/exception/GlobalExceptionHandler.java
@@ -1,22 +1,17 @@
 package com.dhatvibs.dashboard.exception;
 
-<<<<<<< HEAD
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDateTime;
 import java.util.Map;
-=======
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
->>>>>>> c35f69c65ed7288325d8e423c4940a6c0e61612c
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-<<<<<<< HEAD
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception ex){
 
@@ -27,7 +22,7 @@ public class GlobalExceptionHandler {
                 )
         );
     }
-=======
+
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
 
@@ -40,5 +35,5 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
 
->>>>>>> c35f69c65ed7288325d8e423c4940a6c0e61612c
+
 }
